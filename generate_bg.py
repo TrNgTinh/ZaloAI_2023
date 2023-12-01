a93977c2caae46f22499bf589e6362c2e4c38b7e
tinhtn <tinhtranngoc@tech.admicro.vn>
2023-12-01 10:40:09 +0700

    feat: add gen_background_pipeline

--- generate_bg.py
+++ generate_bg.py
@@ -10,49 +10,26 @@ SD_path = current_path / 'SD_Thang'
 sys.path.append(str(SD_path))
 from SD_Thang.sd import BackgroundGenerator
 
-#from Translate_Vi2En.vi2en import TranslatorModule
-
-bg = BackgroundGenerator()
-
-
-# Load data from the CSV file
-csv_file = '/home/tinhtn/tinhtn/Banner/Zalo/Data/info_thuoc_en.csv'
-data = pd.read_csv(csv_file)
-
-
-# Biến để theo dõi số lượng ảnh đã xử lý
-image_count = 0
-
-# Biến để tính tổng thời gian
-total_time = 0
-
-for index, row in data.iterrows():
-    # Kiểm tra xem đã xử lý đủ 100 ảnh chưa
-    if image_count >= 2:
-        break
-    
-    start_time = time.time()
-    
-    # Các bước xử lý ảnh và dịch ngôn ngữ ở đây
-    
-    output_filename = f"{row['bannerImage']}"
-
+def process_image_for_row(row, output_directory="Output/out_bg/"):
     # Process the image using the prompt and save it
     prompt = row['caption_en']
-    bg.process_image(prompt, "Output/out_bg/" + output_filename)
-    
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    total_time += elapsed_time
-    
-    # Tăng biến đếm số lượng ảnh đã xử lý
-    image_count += 1
-    
-    print(f"Processed {output_filename} in {elapsed_time:.2f} seconds")
+    output_filename = f"{row['bannerImage']}"
+    bg.process_image(prompt, output_directory + output_filename)
+    return elapsed_time
 
-# Tính trung bình thời gian chạy
-average_time = total_time / image_count if image_count > 0 else 0
-print(f"Average processing time for {image_count} images: {average_time:.2f} seconds")
+class BackgroundProcessor:
+    def __init__(self, output_directory="Output/out_bg/"):
+        self.output_directory = output_directory
+        self.bg = BackgroundGenerator()
+
+    def process_image_for_row(self, row):
+        
+        # Process the image using the prompt and save it
+        prompt = row['caption_en']
+        output_filename = f"{row['bannerImage']}"
+        self.bg.process_image(prompt, self.output_directory + output_filename)
+        
+
 
 
 
--- predict.py
+++ predict.py
@@ -1,6 +1,8 @@
 from translate_info import build_faiss_index, embed_single_value, relatedness_fn, TranslatorProcessor
 import pandas as pd
 
+from generate_bg import BackgroundProcessor
+
 def predict(row):
     pass
 
@@ -11,12 +13,13 @@ cate_file = '/data/tinhtn/Banner/Zalo/Data/test/loc.txt'
 
 
 translator_processor = TranslatorProcessor(input_csv_file, cate_file)
+bg = BackgroundProcessor(output_directory="Output/out_bg/")
 
 df = pd.read_csv(input_csv_file)
 
 for i, row in df.iterrows():
     row = translator_processor.categorical_row(row, cate_file, output_csv_file) 
-    print(row)
+    bg.process_image_for_row(row)
     
 
 
